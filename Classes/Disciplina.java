401754b2c0d438be189556bf0980b819e944def1
Bruno Sousa Batista <123198431+brun0m@users.noreply.github.com>
2023-06-09 14:24:15 -0300

    Add files via upload

--- Classes/Disciplina.java
+++ Classes/Disciplina.java
@@ -2,13 +2,49 @@ package Classes;
 
 import java.util.ArrayList;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Disciplina {
     protected String nome;
     protected ArrayList<Aluno> alunos;
-    
-    public Disciplina(String nome, ArrayList<Aluno> alunos) {
+    protected Scanner pos;
+
+    public Disciplina(String nome) {
+    	this.alunos = new ArrayList<>();
         this.nome = nome;
-        this.alunos = alunos;
+    }
+    
+    public void adicionarDisciplina(String m) {
+    	FileWriter fw1;
+    	FileWriter fw2;
+    	try {
+    		fw1 = new FileWriter(this.nome + "Gabarito.txt", true);
+    		fw2 = new FileWriter(this.nome + ".txt",true);
+    		BufferedWriter bw = new BufferedWriter(fw1);
+    		bw.write(m);
+    		bw.newLine();
+    		bw.close();
+    		fw1.close();
+    		fw2.close();
+    	} catch (IOException e){
+    		e.printStackTrace();
+    	}
+    }
+    
+    public void adicionarResposta(Aluno m){
+    	FileWriter fw1;
+    	try {
+    		this.alunos.add(m);
+    		fw1 = new FileWriter(this.nome + ".txt", true);
+    		BufferedWriter bw = new BufferedWriter(fw1);
+    		bw.write(m.getRespostas() + " " + m.getNome());
+    		bw.newLine();
+    		bw.close();
+    		fw1.close();
+    	} catch (IOException e){
+    		e.printStackTrace();
+    	}
     }
 
     public String getNome() {
@@ -18,7 +54,7 @@ public class Disciplina {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+   
     public ArrayList<Aluno> getAlunos() {
         return alunos;
     }
@@ -26,4 +62,5 @@ public class Disciplina {
     public void setAlunos(ArrayList<Aluno> alunos) {
         this.alunos = alunos;
     }
+    
 }
